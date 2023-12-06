de3d3dd1606ae955b873c6965086ea2ba85b73f2
JihanCahya <jihan69cahya@gmail.com>
2023-12-06 19:24:27 +0700

    06/12/2023 - Fix gradle SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.polinema.uas.sipkburengan"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.polinema.uas.sipkburengan"
