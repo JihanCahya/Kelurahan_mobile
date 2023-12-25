0c1b9573db3657c54fa55175f7d3380c6ba43204
JihanCahya <jihan69cahya@gmail.com>
2023-12-25 21:20:04 +0700

    25/12/2023 - Perbaikan menu administrasi

--- app/src/main/java/com/polinema/uas/sipkburengan/PengajuanSuratActivity.kt
+++ app/src/main/java/com/polinema/uas/sipkburengan/PengajuanSuratActivity.kt
@@ -1,13 +1,17 @@
 package com.polinema.uas.sipkburengan
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.firebase.storage.FirebaseStorage
 import com.polinema.uas.sipkburengan.databinding.ActivityPengajuanSuratBinding
 
-class PengajuanSuratActivity : Fragment() {
+class PengajuanSuratActivity : Fragment(), View.OnClickListener {
     private lateinit var b: ActivityPengajuanSuratBinding
     lateinit var thisParent: MainActivity
     lateinit var v: View
@@ -15,6 +19,55 @@ class PengajuanSuratActivity : Fragment() {
         thisParent = activity as MainActivity
         b = ActivityPengajuanSuratBinding.inflate(layoutInflater)
         v = b.root
+
+        b.btnSuratKK.setOnClickListener(this)
+        b.btnSuratKTP.setOnClickListener(this)
+        b.btnSuratKeterangan.setOnClickListener(this)
+
         return v
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btnSuratKK -> {
+                val builder = AlertDialog.Builder(requireContext())
+                builder.setTitle("Surat pengajuan KK")
+                builder.setMessage("Persyaratan surat :\n1. Foto copy KTP\n2. Surat pengantar RT")
+                builder.setPositiveButton("Ajukan") { dialog, _ ->
+                    val intent = Intent(requireContext(), PengajuanKKActivity::class.java)
+                    startActivity(intent)
+                }
+                builder.setNegativeButton("Tutup") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                builder.show()
+            }
+            R.id.btnSuratKTP -> {
+                val builder = AlertDialog.Builder(requireContext())
+                builder.setTitle("Surat pengajuan KTP")
+                builder.setMessage("Persyaratan surat :\n1. Foto copy KK\n2. Foto copy Akta Kelahiran\n3. KIA asli/KTP lama\n4. Surat pengantar RT")
+                builder.setPositiveButton("Ajukan") { dialog, _ ->
+                    val intent = Intent(requireContext(), PengajuanKtpActivity::class.java)
+                    startActivity(intent)
+                }
+                builder.setNegativeButton("Tutup") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                builder.show()
+            }
+            R.id.btnSuratKeterangan -> {
+                val builder = AlertDialog.Builder(requireContext())
+                builder.setTitle("Surat keterangan")
+                builder.setMessage("Persyaratan surat :\n1. Foto copy KK\n2. Foto copy Akta Kelahiran\n3. KIA asli/KTP lama\n4. Surat pengantar RT")
+                builder.setPositiveButton("Ajukan") { dialog, _ ->
+                    val intent = Intent(requireContext(), PengajuanKeteranganActivity::class.java)
+                    startActivity(intent)
+                }
+                builder.setNegativeButton("Tutup") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                builder.show()
+            }
+        }
     }
 }
