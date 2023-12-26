1d9bde6f2f6bdc91ec15401ae29c54476eedbaee
desnathasatria <satriadesnatha@gmail.com>
2023-12-26 22:24:23 +0700

    edit

--- app/src/main/java/com/polinema/uas/sipkburengan/EditSuratActivity.kt
+++ app/src/main/java/com/polinema/uas/sipkburengan/EditSuratActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class EditSuratActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_surat)
+        setContentView(R.layout.activity_edit_keterangan)
     }
 }
