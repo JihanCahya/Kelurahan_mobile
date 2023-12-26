07fea872aa0b18b8181f39fc47c27128146a036a
Tegar Darmawan <tegardar@gmail.com>
2023-12-26 14:06:56 +0700

    tambahan activity_main

---
+++ app/src/main/java/com/polinema/uas/sipkburengan/DetailBantuanAcitivity.kt
@@ -0,0 +1,12 @@
+
+package com.polinema.uas.sipkburengan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailBantuanAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_bantuan_acitivity)
+    }
+}
