76c92c543444180fe32e9093bf74d16690ae8624
JihanCahya <jihan69cahya@gmail.com>
2023-12-26 08:48:27 +0700

    26/12/2023 - Penambahan validasi admin

--- app/src/main/java/com/polinema/uas/sipkburengan/ValidasiAdminActivity.kt
+++ app/src/main/java/com/polinema/uas/sipkburengan/ValidasiAdminActivity.kt
@@ -5,23 +5,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.polinema.uas.sipkburengan.databinding.ActivityValidasiAdminBinding
 
 class ValidasiAdminActivity : Fragment() {
     lateinit var thisParent: DashboardAdminActivity
     private lateinit var b : ActivityValidasiAdminBinding
     lateinit var v: View
+    private lateinit var db: DatabaseReference
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         thisParent = activity as DashboardAdminActivity
         b = ActivityValidasiAdminBinding.inflate(layoutInflater)
         v = b.root
 
-        val sharedPreferences = requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-        val uid = sharedPreferences.getString("UID", "")
-
-        b.tvCoba.setText(uid)
+        db = FirebaseDatabase.getInstance().getReference("pengajuan")
+//        val adapter = ValidasiAdapter(requireContext(), ArrayList())
+//
+//        b.lvValidasi.adapter = adapter
+//        b.lvValidasi.setOnItemClickListener { parent, view, position, id ->
+//            val selectedSurat = adapter.getItem(position)
+//            if (selectedSurat != null){
+//                val builder = AlertDialog.Builder(requireContext())
+//                builder.setTitle("Detail Surat")
+//                builder.setMessage("Nama : ${selectedSurat.id_pengaju}\nSurat : ${selectedSurat.surat}\nJenis Surat : ${selectedSurat.jenisSurat}\nTanggal Pengajuan : ${selectedSurat.tanggalPengajuan}\nStatus : ${selectedSurat.status}")
+//                val status = selectedSurat.status
+//                if (status == "Belum dicek"){
+//
+//                } else if (status == "Belum terpenuhi"){
+//
+//                } else if (status == "Sudah diperbarui"){
+//
+//                } else if (status == "Terpenuhi"){
+//
+//                } else if (status == "Sudah diambil"){
+//
+//                }
+//            }
+//        }
 
         return v
     }
