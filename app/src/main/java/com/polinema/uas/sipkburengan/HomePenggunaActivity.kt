95622bf52ef0314253b14d609e607c52b5e54992
JihanCahya <jihan69cahya@gmail.com>
2023-12-26 20:24:23 +0700

    26/12/2023 - Perbaikan

--- app/src/main/java/com/polinema/uas/sipkburengan/HomePenggunaActivity.kt
+++ app/src/main/java/com/polinema/uas/sipkburengan/HomePenggunaActivity.kt
@@ -1,11 +1,21 @@
 package com.polinema.uas.sipkburengan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.polinema.uas.sipkburengan.databinding.ActivityHomePenggunaBinding
 
-class HomePenggunaActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_pengguna)
+class HomePenggunaActivity : Fragment() {
+    private lateinit var b : ActivityHomePenggunaBinding
+    lateinit var thisParent: MainActivity
+    lateinit var v: View
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        thisParent = activity as MainActivity
+        b = ActivityHomePenggunaBinding.inflate(layoutInflater)
+        v = b.root
+
+        return v
     }
 }
